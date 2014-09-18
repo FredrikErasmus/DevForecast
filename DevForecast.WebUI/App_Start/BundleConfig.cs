7f47b62cf907c2f70452f61c4fb1e9db0c3cfdaa
Fredrik Erasmus <fredrik8009@gmail.com>
2014-09-18 21:24:02 +0200

    Added Web Project

---
+++ DevForecast.WebUI/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace DevForecast.WebUI.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/angular")
+                .Include("~/Scripts/angular.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").Include("~/Scripts.Domain/app.js"));
+        }
+    }
+}
---
+++ DevForecast.WebUI/Scripts.Domain/app.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    var app = angular.module("DevForecast", []);
+
+    var MainController = function ($scope, $http) {
+        $scope.testValue = "testValue";
+    };
+
+    app.controller("MainController", ["$scope", "$http", MainController]);
+
+}());
